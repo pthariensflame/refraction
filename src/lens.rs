5cd7ffeec341d900e5842025f8bd47af9b5b3715
Alexander Ronald Altman <alexanderaltman@me.com>
2016-01-18 18:09:22 -0800

    adfbljkre

--- src/lens.rs
+++ src/lens.rs
@@ -1,16 +1,18 @@
-use std::ops::*;
+use std::ops::Deref;
 use std::marker::PhantomData;
 
 /// The type of all lens families.
-pub trait Lens<S, T, A, B>: Clone {
+pub trait Lens<S, T, A, B> {
     fn get(&self, v: S) -> A;
     
-    fn set(&self, v: S, x: B) -> T;
-    
-    fn modify<F: FnOnce(A) -> B>(&self, v: S, f: F) -> T where S: Clone {
-    	self.set(v.clone(), f(self.get(v)))
+    fn set(&self, v: S, x: B) -> T {
+    	self.modify(v, |_| x)
     }
+    
+    fn modify<F: FnOnce(A) -> B>(&self, v: S, f: F) -> T;
 }
+
+pub type LensS<S, A> = Lens<S, S, A, A>;
 
 pub struct Identity<S, T> {
 	phantom_stst : PhantomData<Fn(S) -> (S, Box<Fn(T) -> T>)>,
@@ -55,22 +57,37 @@ impl<S, T, A, B, V, W, LF: Lens<S, T, A, B>, LS: Lens<A, B, V, W>> Compose<S, T,
 	}
 }
 
-impl<S, T, A, B, V, W, LF: Lens<S, T, A, B>, LS: Lens<A, B, V, W>> Clone for Compose<S, T, A, B, V, W, LF, LS> {
+impl<S, T, A, B, V, W, LF: Lens<S, T, A, B> + Clone, LS: Lens<A, B, V, W> + Clone> Clone for Compose<S, T, A, B, V, W, LF, LS> {
 	fn clone(&self) -> Self {
 		Self::of(self.first.clone(), self.second.clone())
 	}
 }
 
-impl<S: Clone, T, A: Clone, B, V, W, LF: Lens<S, T, A, B>, LS: Lens<A, B, V, W>> Lens<S, T, V, W> for Compose<S, T, A, B, V, W, LF, LS> {
+impl<S, T, A, B, V, W, LF: Lens<S, T, A, B>, LS: Lens<A, B, V, W>> Lens<S, T, V, W> for Compose<S, T, A, B, V, W, LF, LS> {
     fn get(&self, v: S) -> V {
     	self.second.get(self.first.get(v))
     }
     
     fn set(&self, v: S, x: W) -> T {
-    	self.first.set(v.clone(), self.second.set(self.first.get(v), x))
+    	self.first.modify(v, |q| self.second.set(q, x))
     }
     
     fn modify<F: FnOnce(V) -> W>(&self, v: S, f: F) -> T {
-    	self.first.modify(v, |x| self.second.modify(x, f))
+    	self.first.modify(v, |q| self.second.modify(q, f))
     }
+}
+
+impl<S, T, A, B, G: Deref<Target=Fn(B) -> T>> Lens<S, T, A, B> for Fn(S) -> (A, G) {
+	fn get(&self, v: S) -> A {
+		self(v).0
+	}
+	
+	fn set(&self, v: S, x: B) -> T {
+		(self(v).1)(x)
+	}
+	
+	fn modify<F: FnOnce(A) -> B>(&self, v: S, f: F) -> T {
+		let (x, g) = self(v);
+		g(f(x))
+	}
 }
