2a56bd5a80eefc0fb123d1fec9de02ba083f4868
Alexander Ronald Altman <alexanderaltman@me.com>
2016-05-30 20:03:07 -0700

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -6,97 +6,97 @@ use std::marker::PhantomData;
 /// The identity lenticuloid.
 #[derive(Debug)]
 pub struct Identity<S, T> {
-	phantom_ss: PhantomData<Box<Fn(S) -> S>>,
-	phantom_tt: PhantomData<Box<Fn(T) -> T>>,
+    phantom_ss: PhantomData<Box<Fn(S) -> S>>,
+    phantom_tt: PhantomData<Box<Fn(T) -> T>>,
 }
 
 impl<S, T> Identity<S, T> {
-	pub fn mk() -> Self {
-		Identity {
-			phantom_ss: PhantomData,
-			phantom_tt: PhantomData,
-		}
-	}
+    pub fn mk() -> Self {
+        Identity {
+            phantom_ss: PhantomData,
+            phantom_tt: PhantomData,
+        }
+    }
 }
 
 impl<S, T> Clone for Identity<S, T> {
-	fn clone(&self) -> Self {
-		Self::mk()
-	}
+    fn clone(&self) -> Self {
+        Self::mk()
+    }
 }
 
 /// Composition of lenticuloids.
 #[derive(Debug)]
 pub struct Compose<S, T, A, B, V, W, LF, LS: ?Sized> {
-	phantom_st: PhantomData<Box<Fn(S) -> T>>,
-	phantom_ab: PhantomData<Box<Fn(A) -> B>>,
-	phantom_ba: PhantomData<Box<Fn(B) -> A>>,
-	phantom_wv: PhantomData<Box<Fn(W) -> V>>,
-	first: LF,
-	second: LS,
+    phantom_st: PhantomData<Box<Fn(S) -> T>>,
+    phantom_ab: PhantomData<Box<Fn(A) -> B>>,
+    phantom_ba: PhantomData<Box<Fn(B) -> A>>,
+    phantom_wv: PhantomData<Box<Fn(W) -> V>>,
+    first: LF,
+    second: LS,
 }
 
 impl<S, T, A, B, V, W, LF, LS> Compose<S, T, A, B, V, W, LF, LS> {
-	pub fn of(f: LF, s: LS) -> Self {
-		Compose {
-			phantom_st: PhantomData,
-			phantom_ab: PhantomData,
-			phantom_ba: PhantomData,
-			phantom_wv: PhantomData,
-			first: f,
-			second: s
-		}
-	}
+    pub fn of(f: LF, s: LS) -> Self {
+        Compose {
+            phantom_st: PhantomData,
+            phantom_ab: PhantomData,
+            phantom_ba: PhantomData,
+            phantom_wv: PhantomData,
+            first: f,
+            second: s,
+        }
+    }
 }
 
 impl<S, T, A, B, V, W, LF: Clone, LS: Clone + ?Sized> Clone for Compose<S, T, A, B, V, W, LF, LS> {
-	fn clone(&self) -> Self {
-		Self::of(self.first.clone(), self.second.clone())
-	}
+    fn clone(&self) -> Self {
+        Self::of(self.first.clone(), self.second.clone())
+    }
 }
 
 /// The first element of a pair.
 #[derive(Debug)]
 pub struct Fst<A0, A1, B0> {
-	phantom_sa: PhantomData<Box<Fn((A0, A1)) -> A0>>,
-	phantom_bt: PhantomData<Box<Fn(B0) -> (B0, A1)>>,
+    phantom_sa: PhantomData<Box<Fn((A0, A1)) -> A0>>,
+    phantom_bt: PhantomData<Box<Fn(B0) -> (B0, A1)>>,
 }
 
 impl<A0, A1, B0> Fst<A0, A1, B0> {
-	pub fn mk() -> Self {
-		Fst {
-			phantom_sa: PhantomData,
-			phantom_bt: PhantomData,
-		}
-	}
+    pub fn mk() -> Self {
+        Fst {
+            phantom_sa: PhantomData,
+            phantom_bt: PhantomData,
+        }
+    }
 }
 
 impl<A0, A1, B0> Clone for Fst<A0, A1, B0> {
-	fn clone(&self) -> Self {
-		Self::mk()
-	}
+    fn clone(&self) -> Self {
+        Self::mk()
+    }
 }
 
 /// The second element of a pair.
 #[derive(Debug)]
 pub struct Snd<A0, A1, B1> {
-	phantom_sa: PhantomData<Box<Fn((A0, A1)) -> A1>>,
-	phantom_bt: PhantomData<Box<Fn(B1) -> (A0, B1)>>,
+    phantom_sa: PhantomData<Box<Fn((A0, A1)) -> A1>>,
+    phantom_bt: PhantomData<Box<Fn(B1) -> (A0, B1)>>,
 }
 
 impl<A0, A1, B1> Snd<A0, A1, B1> {
-	pub fn mk() -> Self {
-		Snd {
-			phantom_sa: PhantomData,
-			phantom_bt: PhantomData,
-		}
-	}
+    pub fn mk() -> Self {
+        Snd {
+            phantom_sa: PhantomData,
+            phantom_bt: PhantomData,
+        }
+    }
 }
 
 impl<A0, A1, B1> Clone for Snd<A0, A1, B1> {
-	fn clone(&self) -> Self {
-		Self::mk()
-	}
+    fn clone(&self) -> Self {
+        Self::mk()
+    }
 }
 
 /// The inversion of a lenticuloid.
@@ -104,10 +104,10 @@ impl<A0, A1, B1> Clone for Snd<A0, A1, B1> {
 pub struct Invert<L: ?Sized>(pub L);
 
 impl<L: ?Sized> Invert<L> {
-	pub fn deinvert(&self) -> &L {
-		let &Invert(ref res) = self;
-		res
-	}
+    pub fn deinvert(&self) -> &L {
+        let &Invert(ref res) = self;
+        res
+    }
 }
 
 mod lens;
--- src/prism.rs
+++ src/prism.rs
@@ -3,9 +3,9 @@ use super::{Identity, Compose};
 
 /// The supertype of all prism families.
 pub trait Prism<S, T, A, B> {
-	fn try_get(&self, v: S) -> Result<A, T>;
-	
-	fn inject(&self, v: B) -> T;
+    fn try_get(&self, v: S) -> Result<A, T>;
+
+    fn inject(&self, v: B) -> T;
 }
 
 /// The supertype of all simple prisms.
@@ -13,26 +13,27 @@ pub trait PrismS<S, A>: Prism<S, S, A, A> {}
 impl<S, A, L: Prism<S, S, A, A> + ?Sized> PrismS<S, A> for L {}
 
 impl<S, T> Prism<S, T, S, T> for Identity<S, T> {
-	fn try_get(&self, v: S) -> Result<S, T> {
-		Ok(v)
-	}
-	
+    fn try_get(&self, v: S) -> Result<S, T> {
+        Ok(v)
+    }
+
     fn inject(&self, v: T) -> T {
-    	v
+        v
     }
 }
 
-impl<S, T, A, B, V, W, LF: Prism<S, T, A, B>, LS: Prism<A, B, V, W> + ?Sized> Prism<S, T, V, W> for Compose<S, T, A, B, V, W, LF, LS> {
+impl<S, T, A, B, V, W, LF: Prism<S, T, A, B>, LS: Prism<A, B, V, W> + ?Sized>
+    Prism<S, T, V, W> for Compose<S, T, A, B, V, W, LF, LS> {
 	fn try_get(&self, v: S) -> Result<V, T> {
 		match self.first.try_get(v) {
 			Ok(q) => match self.second.try_get(q) {
 				Ok(x) => Ok(x),
-				Err(r) => Err(self.first.inject(r)), 
+				Err(r) => Err(self.first.inject(r)),
 			},
 			Err(w) => Err(w),
 		}
 	}
-	
+
     fn inject(&self, v: W) -> T {
     	self.first.inject(self.second.inject(v))
     }
@@ -40,56 +41,57 @@ impl<S, T, A, B, V, W, LF: Prism<S, T, A, B>, LS: Prism<A, B, V, W> + ?Sized> Pr
 
 #[derive(Debug)]
 pub struct PrismFn<G, H: ?Sized> {
-	pub proj: G,
-	pub inj: H,
+    pub proj: G,
+    pub inj: H,
 }
 
 impl<G, H: ?Sized> PrismFn<G, H> {
-	pub fn fst(&self) -> &G {
-		&(self.proj)
-	}
-	
-	pub fn snd(&self) -> &H {
-		&(self.inj)
-	}
+    pub fn fst(&self) -> &G {
+        &(self.proj)
+    }
+
+    pub fn snd(&self) -> &H {
+        &(self.inj)
+    }
 }
 
-impl<S, T, A, B, G: Deref<Target=Fn(S) -> Result<A, T>>, H: Deref<Target=Fn(B) -> T>> Prism<S, T, A, B> for PrismFn<G, H> {
+impl<S, T, A, B, G: Deref<Target=Fn(S) -> Result<A, T>>, H: Deref<Target=Fn(B) -> T>>
+    Prism<S, T, A, B> for PrismFn<G, H> {
 	fn try_get(&self, v: S) -> Result<A, T> {
 		(self.fst())(v)
 	}
-	
+
 	fn inject(&self, v: B) -> T {
 		(self.snd())(v)
 	}
 }
 
 impl<'l, S, T, A, B, L: Prism<S, T, A, B> + ?Sized> Prism<S, T, A, B> for &'l L {
-	fn try_get(&self, v: S) -> Result<A, T> {
-		(**self).try_get(v)
-	}
-	
-	fn inject(&self, v: B) -> T {
-		(**self).inject(v)
-	}
+    fn try_get(&self, v: S) -> Result<A, T> {
+        (**self).try_get(v)
+    }
+
+    fn inject(&self, v: B) -> T {
+        (**self).inject(v)
+    }
 }
 
 impl<'l, S, T, A, B, L: Prism<S, T, A, B> + ?Sized> Prism<S, T, A, B> for &'l mut L {
-	fn try_get(&self, v: S) -> Result<A, T> {
-		(**self).try_get(v)
-	}
-	
-	fn inject(&self, v: B) -> T {
-		(**self).inject(v)
-	}
+    fn try_get(&self, v: S) -> Result<A, T> {
+        (**self).try_get(v)
+    }
+
+    fn inject(&self, v: B) -> T {
+        (**self).inject(v)
+    }
 }
 
 impl<S, T, A, B, L: Prism<S, T, A, B> + ?Sized> Prism<S, T, A, B> for Box<L> {
-	fn try_get(&self, v: S) -> Result<A, T> {
-		(**self).try_get(v)
-	}
-	
-	fn inject(&self, v: B) -> T {
-		(**self).inject(v)
-	}
+    fn try_get(&self, v: S) -> Result<A, T> {
+        (**self).try_get(v)
+    }
+
+    fn inject(&self, v: B) -> T {
+        (**self).inject(v)
+    }
 }
