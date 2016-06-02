5717f45c2e3775286a33a6bc8c9b6f3388709a40
Alexander Ronald Altman <alexanderaltman@me.com>
2016-06-02 09:31:40 -0700

    viv

--- src/refs.rs
+++ src/refs.rs
@@ -1,5 +1,9 @@
+use std::marker::PhantomData;
 use std::ops::{DerefMut, IndexMut};
 use super::{Lens, Lenticuloid};
 
-// ///
-// pub struct ToBorrowed<T: ToOwned>
+///
+pub struct ToBorrowed<B: ToOwned> {
+  phantom_bo: PhantomData<Fn(B) -> B::Owned>,
+  phantom_ob: PhantomData<Fn(B::Owned) -> B>,
+}
